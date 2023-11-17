ad2df2148acf25fc038cd407f3b7fa2e0f23d3b9
Jack Gong <jackgong48@gmail.com>
2023-11-17 16:16:39 +1300

    ADDED: events page

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,31 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const programsCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string(),
-    hasBadge: z.boolean().default(false),
-    image: image(),
-    showOnHomepage: z.boolean().default(false),
-  })
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      hasBadge: z.boolean().default(false),
+      image: image(),
+      showOnHomepage: z.boolean().default(false),
+    }),
+});
+
+const eventsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      address: z.string(),
+      image: image(),
+      hasBadge: z.boolean().default(false),
+      date: z.date(),
+    }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   programs: programsCollection,
+  events: eventsCollection,
 };
